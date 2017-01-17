3e3f46fff157fc8dd87346884cf15a0954b83b55
Madison Melendez <artgeekallday2001@gmail.com>
2017-01-17 21:42:53 +0000

    pixel art

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //Write your function calls below this line
+/*
+Smiley face
+setColor(2, 2, "black");
+setColor(3, 2, "black");
+setColor(1, 6, "black");
+setColor(2, 6, "black");
+setColor(3, 6, "black");
+setColor(7, 4, "black");
+setColor(7, 3, "black");
+setColor(7, 5, "black");
+setColor(6, 2, "black");
+*/
+/*
+letter S
+setColor(0, 7, "black");
+setColor(0, 6, "black");
+setColor(0, 5, "black");
+setColor(1, 4, "black");
+setColor(2, 4, "black");
+setColor(3, 5, "black");
+setColor(3, 6, "black");
+setColor(3, 7, "black");
+setColor(4, 8, "black");
+setColor(5, 8, "black");
+setColor(6, 7, "black");
+setColor(6, 6, "black");
+setColor(6, 5, "black");
+setColor(5, 4, "black");
+setColor(1, 8, "black");
+*/
+
+
+
